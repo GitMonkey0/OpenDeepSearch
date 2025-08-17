ac78bebafedd9609d61ee9aadf541e526fbd1b28
GitMonkey0 <haotianlu@2c.org>
2025-08-17 22:24:34 +0800

    1

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -659,14 +659,14 @@ class RayPPOTrainer(object):
         """
         logger = self.logger
         self.global_steps = 0
-        # # perform validation before training
-        # # currently, we only support validation using the reward_function.
-        # if self.val_reward_fn is not None and self.config.trainer.get('val_before_train', True):
-        #     val_metrics = self._validate()
-        #     pprint(f'Initial validation metrics: {val_metrics}')
-        #     logger.log(data=val_metrics, step=self.global_steps)
-        #     if self.config.trainer.get('val_only', False):
-        #         return
+        # perform validation before training
+        # currently, we only support validation using the reward_function.
+        if self.val_reward_fn is not None and self.config.trainer.get('val_before_train', True):
+            val_metrics = self._validate()
+            pprint(f'Initial validation metrics: {val_metrics}')
+            logger.log(data=val_metrics, step=self.global_steps)
+            if self.config.trainer.get('val_only', False):
+                return
 
         # we start from step 1
         self.global_steps += 1
